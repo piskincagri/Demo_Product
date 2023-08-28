dbd96610a386ba7be18f1b6a10659da35ff438a6
cagri piskin <cagri33@hotmail.com>
2023-08-29 00:24:52 +0300

    Ürünler projesi tamamlandı...

--- Demo_Product/Controllers/ProductController.cs
+++ Demo_Product/Controllers/ProductController.cs
@@ -1,6 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.FluentValidation;
+using DataAccessLayer.Repositories.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,10 +15,70 @@ namespace Demo_Product.Controllers
     public class ProductController : Controller
     {
 
-
+        ProductManager productManager = new ProductManager(new EfProductDAL());
         public IActionResult Index()
         {
+
+            var values = productManager.TGetList();
+            return View(values);
+        }
+         [HttpGet]
+        public IActionResult AddProduct()
+        {
+
             return View();
         }
+        [HttpPost]
+        public IActionResult AddProduct(Product p)
+        {
+            ProductValidator validationRules = new ProductValidator();
+
+            ValidationResult results = validationRules.Validate(p);
+
+            if (results.IsValid)
+            {
+                productManager.TInsert(p);
+
+                return RedirectToAction("Index");
+
+
+
+            }
+            else
+            {
+                foreach(var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+            
+        }
+
+        public IActionResult DeleteProduct(int id)
+        {
+            var values = productManager.TGetById(id);
+            productManager.TDelete(values);
+
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateProduct(int id)
+        {
+            var value = productManager.TGetById(id);
+
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult UpdateProduct(Product p)
+        {
+
+           
+            productManager.TUpdate(p);
+
+            return RedirectToAction("Index");
+
+        }
+        
     }
 }
